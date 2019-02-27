41a9c42e141b7d4dbb63d58d994f898e75c3c22c
Ben Rose <bcrose@bsu.edu>
2019-02-26 20:47:24 -0500

    Added a tour form and connected it to the database

--- Travel-Content-Management-Editor/Controllers/FormController.cs
+++ Travel-Content-Management-Editor/Controllers/FormController.cs
@@ -7,7 +7,7 @@ using Travel_Content_Management_Editor.Models;
 
 namespace Travel_Content_Management_Editor.Controllers
 {
-    public class Places : Controller
+    public class FormController : Controller
     {
         // GET: Form
         public ActionResult Index()
@@ -38,6 +38,31 @@ namespace Travel_Content_Management_Editor.Controllers
 
             };
             context.TOUR_PLACE.Add(place);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home", "Places");
+        }
+        [HttpPost]
+        public ActionResult TourForm(decimal tourId, string tourName, string summary, string tourDescription, string url, string bookNowUrl, string duration, string cost, string groupSize, string additionalInfo)
+        {
+            var context = new TravelContentManagerEntities();
+            var tour = new TOUR()
+            {
+                TOUR_ID = tourId,
+                TOUR_GUID = System.Guid.NewGuid(),
+                TOUR_NAME = tourName,
+                TOUR_SUMMARY = summary,
+                TOUR_DESC = tourDescription,
+                URL = url,
+                BOOK_NOW_URL = bookNowUrl,
+                DURATION = duration,
+                COST = cost,
+                GROUP_SIZE = groupSize,
+                ADDITIONAL_INFO = additionalInfo,
+                ENTERED_BY = System.Guid.NewGuid(),
+                ENTERED_DATE_TIME = DateTime.Now
+
+            };
+            context.TOURs.Add(tour);
             context.SaveChanges();
             return RedirectToAction("Index", "Home", "Places");
         }
