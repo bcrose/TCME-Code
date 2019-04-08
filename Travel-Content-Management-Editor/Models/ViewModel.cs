27f6b50f24f002afc6b009eb2dea3b1d84caab28
Ben Rose <bcrose@bsu.edu>
2019-04-07 22:03:47 -0400

    Got dropdown menus working

--- Travel-Content-Management-Editor/Models/ViewModel.cs
+++ Travel-Content-Management-Editor/Models/ViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Travel_Content_Management_Editor.Models
 {
@@ -10,5 +11,6 @@ namespace Travel_Content_Management_Editor.Models
         public IEnumerable<TOUR_ORGANIZATION_TO_ITEM> orgToItem { get; set; }
         public IEnumerable<TOUR> tour {get; set;}
         public IEnumerable<TOUR_PLACE> place { get; set; }
+        public IEnumerable<SelectListItem> relatedPlaces { get; set; }
     }
 }
